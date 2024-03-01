fd9e448fad8b7a7963757025310d59a716abb517
Zariah <zaripull18@gmail.com>
2024-03-01 16:01:52 -0600

    adding shapes n colors

---
+++ index.js
@@ -0,0 +1,68 @@
+const inquirer = require("inquirer")
+const Circle = require('./lib/Circle') 
+const Square = require("./lib/Square")
+const Triangle = require("./lib/Triangle")
+const SVG = require("./lib/SVG")
+const fs = require("fs")
+
+function acceptPrompt(){
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"enter logo text.",
+            name:"text"
+        },
+        {
+            type:"list",
+            message:"enter logo text.",
+            name:"textcolor",
+            choices:["Red","Green","Purple","Blue","Grey","Pink"]
+        },
+        {
+            type:"list",
+            message:"select a shape",
+            name:"shape",
+            choices:["Circle","Square","Triangle"]
+        },
+        {
+            type:"list",
+            message:"select shape color",
+            name:"shapecolor",
+            choices:["Red","Green","Purple","Blue","Grey","Pink"]
+        },
+    ]).then(response => {
+        switch(response.shape){
+
+        case "Circle":
+            var myLogoShape = new Circle()
+            myLogoShape.setColor(response.shapecolor);
+            generateSVG(myLogoShape, response);
+            break;
+        case "Triangle":
+            var myLogoShape = new Triangle()
+            myLogoShape.setColor(response.shapecolor);
+            generateSVG(myLogoShape, response);
+            break;
+        case "Square":
+            var myLogoShape = new Square()
+            myLogoShape.setColor(response.shapecolor);
+            generateSVG(myLogoShape, response);
+            break;
+        }
+    })
+}
+
+function generateSVG(myLogoShape, response){
+    var renderTextShape = myLogoShape.render()
+    var mySVG = new SVG()
+    mySVG.makeshapeElement(renderTextShape)
+    mySVG.maketextElement(response.text, response.textcolor)
+    var svgHTML = mySVG.render()
+    console.log(svgHTML)
+    fs.writeFileSync("logo.svg",svgHTML,function(err){
+        if(err) throw err;
+    })
+
+}
+
+acceptPrompt()
